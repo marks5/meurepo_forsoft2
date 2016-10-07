1f059fa153018dc9d363efdea7913c8958d58830
marks5 <marks5>
2016-10-07 11:06:27 -0300

    Refatorando código, adicionado controllers vazios.

---
+++ Forsoft2_tentativa6/Forsoft2_tentativa6/Controllers/RecursoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Forsoft2_tentativa6.Controllers
+{
+    public class RecursoController : Controller
+    {
+        // GET: Recurso
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Forsoft2_tentativa6/Forsoft2_tentativa6/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Forsoft2_tentativa6.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
